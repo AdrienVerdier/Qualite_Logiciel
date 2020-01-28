bd8887c1820ab5502a0288f062c93fac4164c8e4
PierreSavary1 <pierre.savary1@gmail.com>
2020-01-28 14:54:16 +0100

    petite modif mainCreation + readMe et pom pour le jar
    pour le jar: à tester

--- Eclipse/UltimateStocker2020/src/main/java/main/MainCreation.java
+++ Eclipse/UltimateStocker2020/src/main/java/main/MainCreation.java
@@ -18,14 +18,20 @@ public class MainCreation {
 		Rayon Rayon2 = new Rayon(2,"Rayon2",null,null,ChefMagasin1);
 		Rayon Rayon3 = new Rayon(3,"Rayon3",null,null,ChefMagasin1);
 		
-		ChefRayon ChefRayon1 = new ChefRayon(1,"nom1","prenom1","password",ChefMagasin1,Rayon1);
-		ChefRayon ChefRayon2 = new ChefRayon(2,"nom2","prenom2","password",ChefMagasin1,Rayon2);
-		ChefRayon ChefRayon3 = new ChefRayon(3,"nom3","prenom3","password",ChefMagasin1,Rayon3);
-		ChefRayon ChefRayon4 = new ChefRayon(4,"nom4","prenom4","password",ChefMagasin1,Rayon3);
+		ChefRayon ChefRayon1 = new ChefRayon(1,"nom1","prenom1","password1",ChefMagasin1,Rayon1);
+		ChefRayon ChefRayon2 = new ChefRayon(2,"nom2","prenom2","password2",ChefMagasin1,Rayon2);
+		ChefRayon ChefRayon3 = new ChefRayon(3,"nom3","prenom3","password3",ChefMagasin1,Rayon3);
+		ChefRayon ChefRayon4 = new ChefRayon(4,"nom4","prenom4","password4",ChefMagasin1,Rayon3);
 		
-		Produit Produit1 = new Produit(1,10,10,"description",Rayon1);
-		Produit Produit2 = new Produit(2,100,0,"description",Rayon1);
-		Produit Produit3 = new Produit(3,50,5,"description",Rayon2);
+		Produit Produit1 = new Produit(1,10,10,"descriptionProduit1",Rayon1);
+		Produit Produit2 = new Produit(2,100,0,"descriptionProduit2",Rayon1);
+		Produit Produit3 = new Produit(3,50,5,"descriptionProduit3",Rayon2);
+		Produit Produit4 = new Produit(4,3,150,"descriptionProduit4",Rayon2);
+		Produit Produit5 = new Produit(5,5,7,"descriptionProduit5",Rayon2);
+		Produit Produit6 = new Produit(6,11,60,"descriptionProduit6",Rayon2);
+		Produit Produit7 = new Produit(7,13,30,"descriptionProduit7",Rayon3);
+		Produit Produit8 = new Produit(8,47,23,"descriptionProduit8",Rayon3);
+		Produit Produit9 = new Produit(9,33,12,"descriptionProduit9",Rayon3);
 		
 		ChefMagasinDAO.ajouterChefMagasin(ChefMagasin1);
 
@@ -52,11 +58,23 @@ public class MainCreation {
 		RayonDAO.ajouterRayonProduit(Rayon1, Produit1);
 		RayonDAO.ajouterRayonProduit(Rayon1, Produit2);
 		RayonDAO.ajouterRayonProduit(Rayon2, Produit3);
+		RayonDAO.ajouterRayonProduit(Rayon2, Produit4);
+		RayonDAO.ajouterRayonProduit(Rayon2, Produit5);
+		RayonDAO.ajouterRayonProduit(Rayon2, Produit6);
+		RayonDAO.ajouterRayonProduit(Rayon3, Produit7);
+		RayonDAO.ajouterRayonProduit(Rayon3, Produit8);
+		RayonDAO.ajouterRayonProduit(Rayon3, Produit9);
 		ProduitDAO.ajouterProduit(Produit1);
 		ProduitDAO.ajouterProduit(Produit2);
 		ProduitDAO.ajouterProduit(Produit3);
+		ProduitDAO.ajouterProduit(Produit4);
+		ProduitDAO.ajouterProduit(Produit5);
+		ProduitDAO.ajouterProduit(Produit6);
+		ProduitDAO.ajouterProduit(Produit7);
+		ProduitDAO.ajouterProduit(Produit8);
+		ProduitDAO.ajouterProduit(Produit9);
 
-		ChefMagasin ChefMagasin2 = new ChefMagasin(1,"test","modif","wesh",null,null);
+		ChefMagasin ChefMagasin2 = new ChefMagasin(1,"Verdier","Adrien","password",null,null);
 		ChefMagasinDAO.modifierChefMagasin(1,ChefMagasin2);
 		//RayonDAO.supprimerRayon(Rayon1);
 	}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # qualite_logiciel
  projet de qualité logiciel
+
+Pour ce projet, nous avons utilisé hibernate afin de gérer la base de données sur wampserver.
+Pour cela vous devez créer la base de données projetqualitelogiciel en amount sur wampserver. 
+
+Pour créer la base de données, lancer wamp, clique gauche sur son icône, onglet phpmyadmin.
+Puis connecter vous avec l'utilisateur root sans mot de passe.
+Cliquer sur le bouton créer Nouvelle base de données et rentrer : projetqualitelogiciel.
+
+Puis vous pouvez lancer l'application :
+ - Tout d'abord vous devez lancer le .. qui va créer la base de données et placer quelques données pour commencer son utilisation.
+ - Puis vous devez lancer ... qui va lancer l'interface pour accéder à la base de données et effectuer des modifications. Ces modifications
+sont enregistrées dans la base de données et pourront être récupérer au prochain lancement de l'application.
